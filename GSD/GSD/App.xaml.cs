d3ee9afa2272a36f7821795c3d30bc407493c35d
Matthias Specht <sylence@btbsoft.org>
2015-09-02 13:20:19 +0200

    Removed dummy code

--- GSD/GSD/App.xaml.cs
+++ GSD/GSD/App.xaml.cs
@@ -22,39 +22,6 @@ namespace GSD
 
 			new SchemaUpdate( cfg ).Execute( false, true );
 			SessionFactory = cfg.BuildSessionFactory();
-
-			//using( var session = SessionFactory.OpenSession() )
-			//using( var tx = session.BeginTransaction() )
-			//{
-			//	var projectRepository = new ProjectRepository( session );
-			//	var todoRepository = new TodoRepository( session );
-			//	var tagRepository = new TagRepository( session );
-
-			//	var project = new Project { Name = "Testproject" };
-			//	projectRepository.Add( project );
-
-			//	var bugTag = new Tag { Name = "Bug", Project = project };
-			//	var taskTag = new Tag { Name = "Task", Project = project };
-
-			//	tagRepository.Add( bugTag );
-			//	tagRepository.Add( taskTag );
-
-			//	project.Tags.Add( bugTag );
-			//	project.Tags.Add( taskTag );
-			//	projectRepository.Update( project );
-
-			//	var todo = new Todo { Summary = "TestBug", Project = project, Details = "This is a test" };
-			//	todoRepository.Add( todo );
-
-			//	todo.Tags.Add( bugTag );
-			//	todoRepository.Update( todo );
-
-			//	project.Todos.Add( todo );
-
-			//	projectRepository.Update( project );
-
-			//	tx.Commit();
-			//}
 		}
 
 		public static ISessionFactory SessionFactory { get; private set; }
